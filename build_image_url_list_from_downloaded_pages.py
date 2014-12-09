d4d52c5584527938b14537a8b5f42268c6ca63eb
Ivan Chaer <ivan.chaer@gmail.com>
2014-12-09 23:54:39 +0100

    created script to build list of image urls to download

--- build_image_url_list_from_downloaded_pages.py
+++ build_image_url_list_from_downloaded_pages.py
@@ -104,6 +104,10 @@ class Browser(QWebView):
 
         return back_image
 
+    def get_url_from_file_name(self, filename):
+        wine_id = filename.split('/')[1][0:-5]
+        return [wine_url[2] for wine_url in wine_urls if wine_url[1] == wine_id][0]
+
     def retrieve_data(self):
 
         # embed jquery
