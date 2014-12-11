626c3b7875fcdc3e45ccee06a62206d7005f152e
Ivan Chaer <ivan.chaer@gmail.com>
2014-12-12 00:47:38 +0100

    added timer

--- build_image_url_list_from_downloaded_pages.py
+++ build_image_url_list_from_downloaded_pages.py
@@ -109,7 +109,12 @@ class Browser(QWebView):
         try:
             wine_id = filename.split('/')[1][0:-5]
 
-            return [url for wine_url in wine_urls if wine_url.index(1) == wine_id].index(0)
+            url = [wine_url[2] for wine_url in wine_urls if wine_url[1] == wine_id][0:1] or []
+
+            if not url:
+                print >> self.log_file, "Error on %s" % filename
+                
+            return url
         except:
             print >> self.log_file, "Error on %s" % filename
             return ''
